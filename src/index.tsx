8dd7bd63356b347d619f7ac75184c8a0a4e09fd9
noctispine <pinefromw@gmail.com>
2022-03-15 03:16:25 +0300

    added store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import {GlobalStyle} from "./components/Styles/GlobalStyles/index"
+import store from './store';
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
+    <GlobalStyle />
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
