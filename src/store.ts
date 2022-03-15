d7a8b718cad37c8b6998b57a376bb50a50b383da
noctispine <pinefromw@gmail.com>
2022-03-15 03:16:02 +0300

    feat: created store

---
+++ src/store.ts
@@ -0,0 +1,17 @@
+import { applyMiddleware, createStore } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import logger from 'redux-logger'
+
+import rootReducer from './redux/reducers/rootReducer'
+import rootSaga from './redux/sagas/rootSaga'
+
+
+const sagaMiddleware = createSagaMiddleware()
+
+const middleware = [sagaMiddleware, logger]
+
+const store = createStore(rootReducer, applyMiddleware(...middleware))
+
+sagaMiddleware.run(rootSaga)
+
+export default store
