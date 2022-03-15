37791d8a2d75c88127a7d8570bfd694cf06beeeb
noctispine <pinefromw@gmail.com>
2022-03-15 03:13:39 +0300

    add theme & baseURL constant

---
+++ src/constants/index.tsx
@@ -0,0 +1,15 @@
+interface ITheme {
+    primary: string,
+    secondary: string,
+    htmlBackground: string,
+
+}
+
+
+export const theme: ITheme = {
+    primary: "#1EA4CE",
+    secondary: "#147594",
+    htmlBackground: "#E5E5E5",
+}
+
+export const baseURL: string = "http://localhost:3004"
