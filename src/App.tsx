e3b8cd042c57fcf10fc5864680527620baffda9f
noctispine <pinefromw@gmail.com>
2022-03-15 03:15:45 +0300

    feat: add some components to verify main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import {AppLayout} from "./components/Styles/Layout"
+import Header from "./components/Header/index"
+import ProductList from "./components/ProductList";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { productActionCreators } from "./redux/reducers/products/reducer";
+import { RootState } from "./redux/reducers/rootReducer";
+
 function App() {
+
+  const dispatch = useDispatch()
+  const products = useSelector((state: RootState) => state.product)
+
+  useEffect(() => {
+    dispatch(productActionCreators.fetchStart())
+  }, [dispatch])
+
+
+  console.log(products)
+  
   return (
-    <div className="App">
-      React Case
-    </div>
+    <AppLayout> 
+      <Header />
+      <ProductList />
+    </AppLayout>
   );
 }
 
