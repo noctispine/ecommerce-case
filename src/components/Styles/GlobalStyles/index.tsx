f8e2fb01cb22c612675b0825f6742abb01523b5b
noctispine <pinefromw@gmail.com>
2022-03-16 14:36:00 +0300

    feat: add font-family

--- src/components/Styles/GlobalStyles/index.tsx
+++ src/components/Styles/GlobalStyles/index.tsx
@@ -1,7 +1,9 @@
 import { createGlobalStyle } from 'styled-components'
 import { normalize } from 'styled-normalize'
+import { theme } from '../../../constants'
  
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
 
+  font-family: ${theme.fontFamily};
 `
