dd38d9a1ec81a9d16e45ff7c060cb44dc58a2446
noctispine <pinefromw@gmail.com>
2022-03-16 14:37:18 +0300

    feat: add some colors

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,15 +1,22 @@
 interface ITheme {
-    primary: string,
-    secondary: string,
-    htmlBackground: string,
-
+  fontFamily: string
+  primary: string
+  secondary: string
+  gray: string
+  lightGray: string
+  smokeyGray: string
+  htmlBackground: string
 }
-
 
 export const theme: ITheme = {
-    primary: "#1EA4CE",
-    secondary: "#147594",
-    htmlBackground: "#E5E5E5",
+  fontFamily: 'Open Sans',
+  primary: '#1EA4CE',
+  secondary: '#147594',
+  gray: '#c4c4c4',
+  lightGray: '#F3F0FE ',
+  smokeyGray: '#697488',
+  htmlBackground: '#FAFAFA',
 }
 
-export const baseURL: string = "http://localhost:3004"
+export const baseURL: string = 'http://localhost:3004'
+export const defaultItemTypes: string[] = ["mug", "shirt"] 
