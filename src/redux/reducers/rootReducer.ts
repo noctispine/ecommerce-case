e48ac5890514b614b20f4fd72f4f4dcbe458ad40
noctispine <pinefromw@gmail.com>
2022-03-17 17:39:50 +0300

    feat: add filter and sort reducer to root

--- src/redux/reducers/rootReducer.ts
+++ src/redux/reducers/rootReducer.ts
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux'
 import companiesReducer from './companies/reducer'
+import filterReducer from './filters/reducer'
 import productsReducer from './products/reducer'
+import sortReducer from './sort/reducer'
 
 const rootReducer = combineReducers({
   product: productsReducer,
-  company: companiesReducer
-  
+  company: companiesReducer,
+  filter: filterReducer,
+  sort: sortReducer,
 })
 
 export default rootReducer
