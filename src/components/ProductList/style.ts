8537e9451d01d5a8a4ca504ffd001b528e421654
noctispine <pinefromw@gmail.com>
2022-03-17 17:42:05 +0300

    feat: add itemType filtering

--- src/components/ProductList/style.ts
+++ src/components/ProductList/style.ts
@@ -1,24 +1,46 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import { theme } from '../../constants'
 
 export const ProductListWrapper = styled.div`
-    width: 100%;
-    height: 100%;
-    height: 67rem;
-    grid-column: 5 / span 4;
-    background: #FFFFFF;
-    
-    display: flex;
-    flex-direction: column;
+  width: 100%;
+  height: 100%;
+  height: 67rem;
+  grid-column: 5 / span 4;
+  margin-top: 2.4rem;
+  display: flex;
+  flex-direction: column;
 `
 
 export const ProductContainer = styled.div`
-    height: 63rem;
+  height: 63rem;
+  background: #ffffff;
+  padding: 1rem 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-
-
-    gap: 1.2rem 1rem;
+  gap: 1.2rem 1rem;
+`
+export const ItemTypeContainer = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  display: flex;
+  gap: 0 0.5rem;
 `
 
+interface IButtonProps {
+  isSelected: boolean
+}
+
+export const Button = styled.button<IButtonProps>`
+  padding: 0.375rem 1rem;
+  color: ${(props) => (props.isSelected ? theme.lightGray : theme.primary)};
+  background-color: ${(props) =>
+    props.isSelected ? theme.primary : theme.lightGray};
+  border: none;
+`
+
+export const Title = styled.div`
+  font-size: 1.25rem;
+  color: ${theme.productListTitleBlack};
+`
