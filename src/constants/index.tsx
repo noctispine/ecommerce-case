554edc986ba85456e17b4d2f0d8d3f205c37ab35
noctispine <pinefromw@gmail.com>
2022-03-17 17:39:27 +0300

    docs: add filter container colors & sort constants

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -4,6 +4,10 @@ interface ITheme {
   secondary: string
   gray: string
   lightGray: string
+  lightGrayText: string
+  blackFilterContainerText: string
+  productListTitleBlack: string
+  outlineGray: string
   smokeyGray: string
   htmlBackground: string
 }
@@ -14,9 +18,25 @@ export const theme: ITheme = {
   secondary: '#147594',
   gray: '#c4c4c4',
   lightGray: '#F3F0FE ',
+  lightGrayText: '#A8A8A8',
+  blackFilterContainerText: '#525252',
+  productListTitleBlack: '#6F6F6F',
+  outlineGray: '#E5E5E5',
   smokeyGray: '#697488',
   htmlBackground: '#FAFAFA',
 }
 
 export const baseURL: string = 'http://localhost:3004'
-export const defaultItemTypes: string[] = ["mug", "shirt"] 
+export const defaultItemTypes: string[] = ['mug', 'shirt']
+
+export const PRICE_LOW_TO_HIGH = 'PRICE_LOW_TO_HIGH'
+export const PRICE_HIGH_TO_LOW = 'PRICE_HIGH_TO_LOW'
+export const NEW_TO_OLD = 'NEW_TO_OLD'
+export const OLD_TO_NEW = 'OLD_TO_NEW'
+
+export const defaultSortTypes = [
+  PRICE_LOW_TO_HIGH,
+  PRICE_HIGH_TO_LOW,
+  NEW_TO_OLD,
+  OLD_TO_NEW,
+]
