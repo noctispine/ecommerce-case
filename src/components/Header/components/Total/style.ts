2ce84d5123514b0a2791df2582ce88b69cc0e42c
noctispine <pinefromw@gmail.com>
2022-03-18 21:49:23 +0300

    feat: add total styles

--- src/components/Header/components/Total/style.ts
+++ src/components/Header/components/Total/style.ts
@@ -3,5 +3,29 @@ import { theme } from '../../../../constants'
 
 export const TotalContainer = styled.div`
   grid-column-end: 12;
+  width: 8rem;
   background-color: ${theme.secondary};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    column-gap: .7rem;
+
+    span {
+      color: #ffffff;
+      height: fit-content;
+    }
+  
+  
+    img {
+      width: 1.1rem;
+      height: 1.1rem;
+    }
+  }
 `
