b76b3c5ae39efff3d3c6fff99c92a1f9d2a90c5e
noctispine <pinefromw@gmail.com>
2022-03-18 21:50:35 +0300

    feat: add bag icon

--- src/components/Header/components/Total/Total.tsx
+++ src/components/Header/components/Total/Total.tsx
@@ -1,10 +1,20 @@
 import { TotalContainer } from './style'
-
+import bag from '../../../../images/bag.svg'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../../../redux/reducers/rootReducer'
 type Props = {}
 
 const Total = (props: Props) => {
+  const cartState = useSelector((state: RootState) => state.cart)
+
+
   return (
-    <TotalContainer>t 142.12</TotalContainer>
+    <TotalContainer>
+      <div>
+        <img src={bag} alt="bag" />
+        <span>₺ {cartState.total}</span>
+      </div>
+    </TotalContainer>
   )
 }
 
