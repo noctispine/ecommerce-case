383b20832cdbe79558de8a28bdae973cfdea20b3
noctispine <pinefromw@gmail.com>
2022-03-18 21:48:43 +0300

    feat: add logo & bag icon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import Total from './components/Total/Total'
 import { HeaderContainer, TitleContainer } from './style'
+import logo from '../../images/logo.png'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
     <HeaderContainer>
-      <TitleContainer>MARKET</TitleContainer>
+      <TitleContainer>
+        <img src={logo} alt="logo" />
+      </TitleContainer>
       <Total />
     </HeaderContainer>
   )
