d5e627fc66d86e0a1733e9eefe9b62bcc30cd3d9
noctispine <pinefromw@gmail.com>
2022-03-18 17:58:58 +0300

    refactor: with shared components

--- src/components/Filters/components/Tags/Tags.tsx
+++ src/components/Filters/components/Tags/Tags.tsx
@@ -5,10 +5,8 @@ import { RootState } from '../../../../redux/reducers/rootReducer'
 import {
   FilterContainer,
   InputContainer,
-  ListContainer,
-  SearchInput,
 } from '../../style'
-import TagItem from './component/TagItem'
+import SharedSearchFilter from '../SharedComponents/SharedSearchFilter'
 
 type Props = {}
 
@@ -19,65 +17,18 @@ const Tags = (props: Props) => {
   const filterState = useSelector((state: RootState) => state.filter)
   const tagFilters = filterState.tags
 
-  const dispacth = useDispatch()
-
-  const [searchQuery, setSearchQuery] = useState('')
-
-  const handleAddFilterTag = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const input = e.target
-    if (input.value === 'All') {
-      dispacth(
-        filterActionCreators.removeAllFiltersFromSpecificTypeCreator('tags')
-      )
-    } else {
-      if (input.checked) {
-        dispacth(filterActionCreators.addFilterTagCreator(input.value))
-      } else {
-        dispacth(filterActionCreators.removeFilterTagCreator(input.value))
-      }
-    }
-  }
+  
 
   return (
     <FilterContainer>
       Tags
       <InputContainer>
-        <SearchInput
-          placeholder="Search Tag"
-          onChange={(e) => setSearchQuery(e.target.value)}
+        <SharedSearchFilter
+          name="Tag"
+          items={tagsWithTotal.tags}
+          total={tagsWithTotal.total}
+          filter={tagFilters}
         />
-        <ListContainer>
-          <TagItem
-            key={tagsWithTotal.total + 1}
-            name="All"
-            quantity={tagsWithTotal.total}
-            isChecked={tagFilters.length === 0}
-            handleAddFilterTag={handleAddFilterTag}
-          />
-          {
-            // first filter wrt search bar
-            // then map them into items
-            tagsWithTotal.tags
-              .filter((tag) => {
-                if (searchQuery === '') return tag
-                else if (
-                  tag.name
-                    .toLocaleLowerCase()
-                    .includes(searchQuery.toLocaleLowerCase())
-                )
-                  return tag
-              })
-              .map((tag, indx) => (
-                <TagItem
-                  key={indx}
-                  name={tag.name}
-                  quantity={tag.quantity}
-                  isChecked={tagFilters.includes(tag.name)}
-                  handleAddFilterTag={handleAddFilterTag}
-                />
-              ))
-          }
-        </ListContainer>
       </InputContainer>
     </FilterContainer>
   )
