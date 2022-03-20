0d436c9dc1a35d70b7e077cc49fa1cc6454f1fd0
noctispine <pinefromw@gmail.com>
2022-03-20 14:53:06 +0300

    fix: delete additional calculation of tags

--- src/redux/sagas/product/index.ts
+++ src/redux/sagas/product/index.ts
@@ -18,7 +18,6 @@ function* onLoadProducts() {
     const response: AxiosResponse = yield call(fetchItems)
 
     yield put(productActionCreators.fetchSuccess(response.data))
-    yield put(productActionCreators.updateTags(response.data))
   } catch (error) {
     if (error instanceof Error) {
       yield put(productActionCreators.fetchFail(error.message))
